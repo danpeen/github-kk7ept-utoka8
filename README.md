302b9e4b260aba44734d9f9f986a33bd966d92a6
fengdanping <fengdanping@bytedance.com>
2022-06-08 17:52:03 +0800

    feat(examples): add README.md

--- README.md
+++ README.md
@@ -18,4 +18,30 @@ Host(app1) is the shell app which imports Remote(app2). Host is hosted on port 3
 
 Remote(app2) is hosted port 3002 and exposes 1 components button.
 
+Remote(app3) is hosted port 3003 and exposes 1 components button.
+
 The exposed components are used in Host.
+
+### Usage
+
+- click `Sub-APP-2`, Garfish will load Sub-APP-2
+- click `Sub-APP-3`, Garfish will load Sub-APP-3
+
+### Main App's feature:
+  -  1) An Micro frontend app with Garfish;
+  -  2) Load Sub App when browser router changed,which Garfish will load the sub app according to `activeWhen` param;
+  -  3) share the common libraries for sub APPs;
+  -  4) expose common common utils func and components for sub APPs;
+  -  5) support hmr in devServer (by react-fast-refresh);
+
+### Sub-APP-2's feature:
+  - 1) share common utils func and components from main-App;
+  - 2) show the exposed component from Sub-App-3;
+  - 3) support hmr in devServer (by react-fast-refresh);
+  - 4) share the common libraries from main App.(such as React、React-dom、React-router-dom); [check the `Network` in console panel]
+
+### Sub-APP-3's feature:
+  - 1) share common utils func and components from main-App;
+  - 2) support hmr in devServer (by react-fast-refresh);
+  - 3) share the common libraries from main App.(such as React、React-dom、React-router-dom); [check the `Network` in console panel]
+  - 4) it has own independent routes, which can test whether the sub-app routes is normal in Garfish;
--- app1/src/App.tsx
+++ app1/src/App.tsx
@@ -15,13 +15,43 @@ const MainHome = () => {
   return (
     <div className="home">
       <h2> welcome, </h2>
-      <h2>
-        Hello, This is a demo mirco frontend projects using Garfish with Module
+      <h3>
+        This is a demo mirco frontend projects using Garfish with Module
         federation.
-      </h2>
+      </h3>
       <h4> 1、click `Sub-APP-2`, Garfish will load Sub-APP-2</h4>
       <h4> 2、click `Sub-APP-3`, Garfish will load Sub-APP-3</h4>
-      <h4> 3、Sub-APP-2：</h4>
+
+      <h4 className="block">
+        3、Main App's feature: <br />
+        1) An Micro frontend app with Garfish; <br />
+        2) Load Sub App when browser router changed,which Garfish will load the
+        sub app according to `activeWhen` param; <br />
+        3) share the common libraries for sub APPs; <br />
+        4) expose common common utils func and components for sub APPs; <br />
+        5) support hmr in devServer (by react-fast-refresh); <br />
+      </h4>
+
+      <h4 className="block">
+        3、Sub-APP-2's feature: <br />
+        1) share common utils func and components from main-App; <br />
+        2) show the exposed component from Sub-App-3; <br />
+        3) support hmr in devServer (by react-fast-refresh); <br />
+        4) share the common libraries from main App.(such as
+        React、React-dom、React-router-dom); [check the `Network` in console
+        panel] <br />
+      </h4>
+
+      <h4 className="block">
+        3、Sub-APP-3's feature: <br />
+        1) share common utils func and components from main-App; <br />
+        2) support hmr in devServer (by react-fast-refresh); <br />
+        3) share the common libraries from main App.(such as
+        React、React-dom、React-router-dom); [check the `Network` in console
+        panel] <br />
+        4) it has own independent routes, which can test whether the sub-app
+        routes is normal in Garfish;
+      </h4>
     </div>
   );
 };
@@ -44,7 +74,7 @@ const Home = () => {
     <div className="main-app">
       <div className="left">
         <div className="title" onClick={() => navigate({ pathname: "/home" })}>
-          <h1> Main App </h1>
+          <h1 className="logo"> Main App </h1>
         </div>
 
         {/* <Suspense fallback={"button loading..."}>
